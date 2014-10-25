260908f87901ffda1b17caa0fabd274d45310616
Fredy <d>
2014-10-25 12:13:42 -0500

    version 3

---
+++ Yamba1-Gr3/src/co/edu/udea/compumovil/gr3/yamba/SettingsActivity.java
@@ -0,0 +1,18 @@
+package co.edu.udea.compumovil.gr3.yamba;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SettingsActivity extends Activity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		// Check whether this activity was created before
+			if (savedInstanceState == null) {
+				// Create a fragment
+				SettingsFragment fragment = new SettingsFragment();
+		getFragmentManager().beginTransaction().add(android.R.id.content, fragment,fragment.getClass().getSimpleName()).commit(); //
+		}
+	};
+}
